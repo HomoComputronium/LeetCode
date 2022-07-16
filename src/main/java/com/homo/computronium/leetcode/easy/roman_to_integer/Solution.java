b1ea7378a3c3854de8a9816cef1cd3dd58b2dd9a
HomoComputronium <fedotenko.aleksey@yandex.com>
2022-07-16 19:35:02 +0500

    Solve 13. Roman to Integer task

---
+++ src/main/java/com/homo/computronium/leetcode/easy/roman_to_integer/Solution.java
@@ -0,0 +1,45 @@
+package com.homo.computronium.leetcode.easy.roman_to_integer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/roman-to-integer/
+// 13. Roman to Integer
+public class Solution {
+
+    private final Map<String, Integer> int2string = new HashMap<>();
+
+    public Solution() {
+        populateMap();
+    }
+
+    public int romanToInt(String roman) {
+        int result = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+            String currentRoman = Character.toString(roman.charAt(i));
+            Integer currentArabic = int2string.get(currentRoman);
+            result += currentArabic;
+
+            if (i != 0) {
+                String prevRoman = Character.toString(roman.charAt(i - 1));
+                Integer prevArabic = int2string.get(prevRoman);
+                if (prevArabic < currentArabic) {
+                    result -= prevArabic; // nullifying the previous addition
+                    result -= prevArabic; // subtracting
+                }
+            }
+        }
+        return result;
+    }
+
+    private void populateMap() {
+        int2string.put("I", 1);
+        int2string.put("V", 5);
+        int2string.put("X", 10);
+        int2string.put("L", 50);
+        int2string.put("C", 100);
+        int2string.put("D", 500);
+        int2string.put("M", 1000);
+    }
+}
---
+++ src/test/java/com/homo/computronium/leetcode/easy/roman_to_integer/SolutionTest.java
@@ -0,0 +1,67 @@
+package com.homo.computronium.leetcode.easy.roman_to_integer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    public void simpleCases() {
+//        I             1
+//        V             5
+//        X             10
+//        L             50
+//        C             100
+//        D             500
+//        M             1000
+
+        Solution solution = new Solution();
+
+        assertEquals(1, solution.romanToInt("I"));
+        assertEquals(5, solution.romanToInt("V"));
+        assertEquals(10, solution.romanToInt("X"));
+        assertEquals(50, solution.romanToInt("L"));
+        assertEquals(100, solution.romanToInt("C"));
+        assertEquals(500, solution.romanToInt("D"));
+        assertEquals(1000, solution.romanToInt("M"));
+    }
+
+    @Test
+    public void casesWithAddingRomans() {
+        Solution solution = new Solution();
+
+        assertEquals(1, solution.romanToInt("I"));
+        assertEquals(2, solution.romanToInt("II"));
+        assertEquals(3, solution.romanToInt("III"));
+
+        assertEquals(6, solution.romanToInt("VI"));
+        assertEquals(7, solution.romanToInt("VII"));
+        assertEquals(8, solution.romanToInt("VIII"));
+
+        assertEquals(26, solution.romanToInt("XXVI"));
+        assertEquals(27, solution.romanToInt("XXVII"));
+        assertEquals(28, solution.romanToInt("XXVIII"));
+    }
+
+    @Test
+    public void casesWithSubtractingRomans() {
+        Solution solution = new Solution();
+
+        assertEquals(4, solution.romanToInt("IV"));
+        assertEquals(9, solution.romanToInt("IX"));
+        assertEquals(14, solution.romanToInt("XIV"));
+
+        assertEquals(19, solution.romanToInt("XIX"));
+        assertEquals(34, solution.romanToInt("XXXIV"));
+    }
+
+    @Test
+    public void nativeLeetCodeTestCases() {
+        Solution solution = new Solution();
+
+        assertEquals(58, solution.romanToInt("LVIII"));
+        assertEquals(1994, solution.romanToInt("MCMXCIV"));
+    }
+
+}
