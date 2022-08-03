4ea39fcc864e2afa380fd38c128d0b498b76b6a1
HomoComputronium <fedotenko.aleksey@yandex.com>
2022-08-03 22:25:37 +0500

    Climbing steps no solution yet

---
+++ src/main/java/com/homo/computronium/leetcode/easy/e0070/Solution0070.java
@@ -0,0 +1,9 @@
+package com.homo.computronium.leetcode.easy.e0070;
+
+// https://leetcode.com/problems/climbing-stairs/
+// 70. Climbing Stairs
+public class Solution0070 {
+    public int climbStairs(int n) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/homo/computronium/leetcode/easy/e0070/Solution0070Test.java
@@ -0,0 +1,7 @@
+package com.homo.computronium.leetcode.easy.e0070;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution0070Test {
+
+}
