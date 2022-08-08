c721e81a84d9dfc4ca75f980d910fc2a7e130a18
HomoComputronium <fedotenko.aleksey@yandex.com>
2022-08-09 00:27:04 +0500

    Solve inorderTreeTraversal problem

---
+++ src/main/java/com/homo/computronium/leetcode/easy/e0094/Solution0094.java
@@ -0,0 +1,30 @@
+package com.homo.computronium.leetcode.easy.e0094;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+// 94. Binary Tree Inorder Traversal
+public class Solution0094 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> valueList = new ArrayList<>();
+        if (Objects.nonNull(root)) {
+            inorderTraversal(root.left, valueList);
+            valueList.add(root.val);
+            inorderTraversal(root.right, valueList);
+        }
+        return valueList;
+    }
+
+    private List<Integer> inorderTraversal(TreeNode root, List<Integer> valueList) {
+        if (Objects.nonNull(root)) {
+            inorderTraversal(root.left, valueList);
+            System.out.println(root.val);
+            valueList.add(root.val);
+            inorderTraversal(root.right, valueList);
+        }
+        return valueList;
+    }
+}
---
+++ src/test/java/com/homo/computronium/leetcode/easy/e0094/Solution0094Test.java
@@ -0,0 +1,57 @@
+package com.homo.computronium.leetcode.easy.e0094;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution0094Test {
+
+    @Test
+    public void simpleTest() {
+        Solution0094 solution = new Solution0094();
+
+//             1
+//            / \
+//           2   3
+//          / \
+//         4   5
+        TreeNode leafLeft = new TreeNode(4);
+        TreeNode leafRight = new TreeNode(5);
+
+        TreeNode right = new TreeNode(3);
+        TreeNode left = new TreeNode(2, leafLeft, leafRight);
+        TreeNode root = new TreeNode(1, left, right);
+
+        List<Integer> expectedList = Arrays.asList(4, 2, 5, 1, 3);
+
+        assertEquals(expectedList, solution.inorderTraversal(root));
+
+    }
+
+    @Test
+    public void nullCheckTest() {
+        Solution0094 solution = new Solution0094();
+        TreeNode root = null;
+
+        List<Integer> expectedList = List.of();
+
+        assertEquals(expectedList, solution.inorderTraversal(root));
+
+    }
+
+    @Test
+    public void leftNullCheckTest() {
+        Solution0094 solution = new Solution0094();
+        TreeNode root = new TreeNode(1, null, null);
+
+        List<Integer> expectedList = List.of(1);
+
+        assertEquals(expectedList, solution.inorderTraversal(root));
+    }
+
+
+
+}
